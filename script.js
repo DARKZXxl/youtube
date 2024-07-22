9c43605d60507f27affbbc9ed4afead7be472e50
DARKZXxl <gabrielfigueira88@gmail.com>
2024-07-21 22:21:14 -0300

    0.3

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function clicar() {
     tudo.style.color = 'white'
     novidades.style.background = 'white'
     novidades.style.color = 'black'
-    novidades.style.transition = '700ms'
+    novidades.style.transition = '800ms'
     
 }
 
@@ -24,6 +24,84 @@ function voltar() {
     novidades.style.background = 'linear-gradient(#000000, #000000)padding-box,linear-gradient(90deg,#30dfd3, #7d3bdf, #e827db, #c022a0, #e53434, #e36b26) border-box'
     novidades.style.border = '1px solid transparent'
     novidades.style.color = 'white'
-    novidades.style.transition = '700ms'
+    tudo.style.transition = '1s'
     
+}
+
+var jogo = document.getElementById('pac')
+var nome = document.getElementById('jogo')
+var por = document.getElementById('porcento')
+
+var mari = document.getElementById('mario')
+var jogo1 = document.getElementById('jogo1')
+var por2 = document.getElementById('porcento1')
+
+var soni = document.getElementById('sonic')
+var jogo2 = document.getElementById('jogo2')
+var por3 = document.getElementById('porcento2')
+
+function qual() {
+    jogo.style.border = '2px solid blue'
+    nome.style.color = 'blue'
+    por.innerHTML = '9%'
+    por.style.color = 'blue'
+    nome.style.transition = '1s'
+    nome.style.backgroundColor = ' #ffffff3e'
+    nome.style.width = '17%'
+    
+    por2.innerHTML = '37%'
+    jogo1.style.width = '30%'
+    jogo1.style.backgroundColor = ' #ffffff3e'
+    jogo1.style.transition = '500ms'
+    por2.style.color = 'white'
+
+    jogo2.style.transition = '1s'
+    jogo2.style.backgroundColor = ' #ffffff3e'
+    jogo2.style.width = '40%'
+    por3.innerHTML = '54%'
+    por3.style.color = 'white'
+}
+
+function mario() {
+    mari.style.border = '2px solid blue'
+    jogo1.style.color = 'blue'
+    por2.innerHTML = '37%'
+    por2.style.color = 'blue'
+    jogo1.style.transition = '500ms'
+    jogo1.style.backgroundColor = ' #ffffff3e'
+    jogo1.style.width = '30%'
+
+    nome.style.transition = '1s'
+    nome.style.backgroundColor = ' #ffffff3e'
+    nome.style.width = '17%'
+    por.innerHTML = '9%'
+    por.style.color = 'white'
+
+    jogo2.style.transition = '1s'
+    jogo2.style.backgroundColor = ' #ffffff3e'
+    jogo2.style.width = '40%'
+    por3.innerHTML = '54%'
+    por3.style.color = 'white'
+}
+
+function sonic() {
+    soni.style.border = '2px solid blue'
+    jogo2.style.color = 'blue'
+    por3.innerHTML = '54%'
+    por3.style.color = 'blue'
+    jogo2.style.transition = '1s'
+    jogo2.style.backgroundColor = ' #ffffff3e'
+    jogo2.style.width = '40%'
+
+    nome.style.transition = '1s'
+    nome.style.backgroundColor = ' #ffffff3e'
+    nome.style.width = '17%'
+    por.innerHTML = '9%'
+    por.style.color = 'white'
+
+    jogo1.style.transition = '500ms'
+    jogo1.style.backgroundColor = ' #ffffff3e'
+    jogo1.style.width = '30%'
+    por2.innerHTML = '37%'
+
 }
